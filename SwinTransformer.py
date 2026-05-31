28eb6251193b2fec87edca76f9f243cac205cc2d
wendian <67248459+wendian02@users.noreply.github.com>
2026-05-31 17:43:17 -0400

    fix swinT

--- SwinTransformer.py
+++ SwinTransformer.py
@@ -1,6 +1,125 @@
 import torch.nn as nn
 import torch
-from utils import SwinEmbedding, PatchMerging, SwinEncoder
+import torch.nn.functional as F
+from einops import rearrange
+from einops.layers.torch import Rearrange
+import numpy as np
+
+
+# https://medium.com/thedeephub/building-swin-transformer-from-scratch-using-pytorch-hierarchical-vision-transformer-using-shifted-91cbf6abc678
+
+
+class SwinEmbedding(nn.Module): # Patch Partition and Linear Embedding
+    def __init__(self, patch_size=4, emb_size=96):
+        super().__init__()
+        self.linear_embedding = nn.Conv2d(
+            3, emb_size, kernel_size=patch_size, stride=patch_size
+        ) # -> B, 96, H/patch_size, W/patch_size
+        self.rearrange = Rearrange("b c h w -> b (h w) c")
+
+    def forward(self, x):
+        x = self.linear_embedding(x)
+        x = self.rearrange(x) # -> B, H/patch_size * W/patch_size, 96
+        return x
+
+class PatchMerging(nn.Module):
+    def __init__(self, emb_size):
+        super().__init__()
+        self.linear = nn.Linear(4*emb_size, 2*emb_size)
+
+    def forward(self, x): # B, L(P*P), C
+        B, L, C = x.shape
+        H = W = int(np.sqrt(L)/2)
+        x = rearrange(x, 'b (h s1 w s2) c -> b (h w) (s1 s2 c)', s1=2, s2=2, h=H, w=W) # B, L(P*P), C -> B, (P/2 * P/2), 2*2*C
+        x = self.linear(x) # B, L(P*P/4), 4*C -> B, L(P*P/4), 2*C
+        return x # B, L(P/2 * P/2), 2*C
+
+
+class ShiftedWindowMSA(nn.Module):
+    def __init__(self, emb_size, num_heads, window_size=7, shifted=True):
+        super().__init__()
+        self.emb_size = emb_size
+        self.num_heads = num_heads
+        self.window_size = window_size
+        self.shifted = shifted
+        self.linear1 = nn.Linear(emb_size, 3*emb_size)
+        self.linear2 = nn.Linear(emb_size, emb_size)
+
+        self.pos_embeddings = nn.Parameter(torch.randn(window_size*2 - 1, window_size*2 - 1))
+        indices = torch.tensor(np.array([[x, y] for x in range(window_size) for y in range(window_size)]))
+        relative_indices = indices[None, :, :] - indices[:, None, :]
+        relative_indices += self.window_size - 1
+        self.register_buffer('relative_indices', relative_indices)
+
+
+    def forward(self, x):
+        h_dim = self.emb_size / self.num_heads
+        height = width = int(np.sqrt(x.shape[1]))
+        x = self.linear1(x)
+        
+        x = rearrange(x, 'b (h w) (c k) -> b h w c k', h=height, w=width, k=3, c=self.emb_size)
+        
+        if self.shifted:
+            x = torch.roll(x, (-self.window_size//2, -self.window_size//2), dims=(1,2))
+        
+        x = rearrange(x, 'b (Wh w1) (Ww w2) (e H) k -> b H Wh Ww (w1 w2) e k', w1 = self.window_size, w2 = self.window_size, H = self.num_heads)            
+        
+        Q, K, V = x.chunk(3, dim=6)
+        Q, K, V = Q.squeeze(-1), K.squeeze(-1), V.squeeze(-1)
+        wei = (Q @ K.transpose(4,5)) / np.sqrt(h_dim)
+        
+        rel_pos_embedding = self.pos_embeddings[self.relative_indices[:, :, 0], self.relative_indices[:, :, 1]]
+        wei += rel_pos_embedding
+        
+        if self.shifted:
+            row_mask = torch.zeros((self.window_size**2, self.window_size**2)).to(x.device)
+            row_mask[-self.window_size * (self.window_size//2):, 0:-self.window_size * (self.window_size//2)] = float('-inf')
+            row_mask[0:-self.window_size * (self.window_size//2), -self.window_size * (self.window_size//2):] = float('-inf')
+            column_mask = rearrange(row_mask, '(r w1) (c w2) -> (w1 r) (w2 c)', w1=self.window_size, w2=self.window_size)
+            wei[:, :, -1, :] += row_mask
+            wei[:, :, :, -1] += column_mask
+        
+        wei = F.softmax(wei, dim=-1) @ V
+        
+        x = rearrange(wei, 'b H Wh Ww (w1 w2) e -> b (Wh w1) (Ww w2) (H e)', w1 = self.window_size, w2 = self.window_size, H = self.num_heads)
+        x = rearrange(x, 'b h w c -> b (h w) c')
+        
+        return self.linear2(x)
+    
+
+class MLP(nn.Module):
+    def __init__(self, emb_size):
+        super().__init__()
+        self.ff = nn.Sequential(
+                         nn.Linear(emb_size, 4*emb_size),
+                         nn.GELU(),
+                         nn.Linear(4*emb_size, emb_size),
+                  )
+    
+    def forward(self, x):
+        return self.ff(x)
+    
+class SwinEncoder(nn.Module):
+    def __init__(self, emb_size, num_heads, window_size=7):
+        super().__init__()
+        self.WMSA = ShiftedWindowMSA(emb_size, num_heads, window_size, shifted=False)
+        self.SWMSA = ShiftedWindowMSA(emb_size, num_heads, window_size, shifted=True)
+        self.ln1 = nn.LayerNorm(emb_size)
+        self.ln2 = nn.LayerNorm(emb_size)
+        self.ln3 = nn.LayerNorm(emb_size)
+        self.ln4 = nn.LayerNorm(emb_size)
+        self.MLP1 = MLP(emb_size)
+        self.MLP2 = MLP(emb_size)
+        
+    def forward(self, x): # -> B, L, C
+        # Window Attention
+        x = x + self.WMSA(self.ln1(x)) # B, L, C
+        x = x + self.MLP1(self.ln2(x)) # B, L, C
+        # shifted Window Attention
+        x = x + self.SWMSA(self.ln3(x)) # B, L, C
+        x = x + self.MLP2(self.ln4(x)) # B, L, C
+        
+        return x # -> B, L, C
 
 class Swin(nn.Module):
     def __init__(self):
